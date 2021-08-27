09712c77f7408bc6ab348f38fe19209a2ab32016
melgress <63718040+melgress@users.noreply.github.com>
2021-08-27 10:54:08 +0200

    calculate total minutes and tracks needed

--- conftrckmng/src/main/Ctm_main.java
+++ conftrckmng/src/main/Ctm_main.java
@@ -5,19 +5,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Ctm_main {
+	
+	
+	
 
 	public static void main(String[] args) {
 		
-		      
+ 
 		try {
 
 			BufferedReader in = new BufferedReader(new FileReader("test3"));
 
 			StringBuilder sb = new StringBuilder();
+	        int totalMinutes = 0;
+
 			
 			String line;
 			String title = null;
 			String time = null;
+		//	String timeLightning = null;
+			Boolean nextExists = null;
+			 Session slot = null;
+
+
+			
 
 
 			 while ((line = in.readLine()) != null) {
@@ -25,36 +36,46 @@ public class Ctm_main {
 				sb.append("\n");
 				
 
-				//lightning rausnehmen
-		        Pattern p1 = Pattern.compile("(^(?!(min|lightning)$)[a-z.A-Z-:()\\s]+)");
+				//lightning aus Titel rausnehmen
+		        Pattern p1 = Pattern.compile("(^(?!(min)$)[a-z.A-Z-:()\\s]+)");
 		        Matcher m1 = p1.matcher(line);
 				Pattern p2 = Pattern.compile("((\\d+))");
 		        Matcher m2 = p2.matcher(line);
+		        Pattern p3 = Pattern.compile("(((\\blightning\\b)))");
+		        Matcher m3 = p3.matcher(line);
 				
 
 		        while(m1.find()) {
 		        	title = m1.group(1);
+		        	nextExists = true;
 		        	
-		            //System.out.println(title);
+		        	if (m2.find()) {
+		        		time = m2.group(2);
+		        		} else 
+		        			time = "5";
+		        	
 		            
 		            
 		        }
-		        while(m2.find()) {
-		        	time = m2.group(2);
-		          
-		        	
-		          //  System.out.println(time);
+
 		            
-		            }
+		            
 		        int timeInt = Integer. parseInt(time);
+		        totalMinutes = totalMinutes + timeInt;
+		        //System.out.println(totalMinutes);
 		        String min = "min";
 		        Talks event = new Talks (title, timeInt, min);
-		       // Session slot = new Session (0, 0);
-		        //slot.addTalk(event);
-			 }
-				
 
+		        
+		       
+		      	
+			 }
+			
+			
+			calculateNumberTracks(totalMinutes);
 			in.close();
+			
+			
 				
 				
 			
@@ -62,6 +83,19 @@ public class Ctm_main {
 		} catch (IOException ex) {
 		    ex.printStackTrace();
 		}
+		
+		
+		
+	}
+	public static void calculateNumberTracks (int totalMinutes) {
+		double availableTrackMins = 480;
+		//int tracksNeeded = 0;
+		double tracksNeeded = totalMinutes/availableTrackMins;
+		int total = (int) Math.ceil((double)tracksNeeded);
+		System.out.println(totalMinutes);
+		System.out.println(tracksNeeded);
+		System.out.println(total);
+		
 	}
 }
 
--- conftrckmng/src/main/Session.java
+++ conftrckmng/src/main/Session.java
@@ -2,6 +2,7 @@ package main;
 //Hier wird ein Session Objekt erzeugt, welches Talks und Zeiten enthält
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.event.ListSelectionEvent;
@@ -9,44 +10,55 @@ import javax.swing.event.ListSelectionEvent;
 public class Session {
 	 
 	List <Talks> events;
+	Talks event;
 	int startTime;
 	int timeNeeded;
 	int durationLeft;
-	Talks event;
+	//Talks event;
 	
 	
-	public Session (int start, int duration)  {
+	public Session (List <Talks> events)  {
 		events = new ArrayList<Talks>();
-		this.startTime = start;
-		this.durationLeft = duration;	
+		
+		
 		
 	}
+
 	
 	/*Session morningSession = new Session(events, 9, 180);
 	Session lunch = new Session (events, 12, 60);
 	Session afternoonSession = new Session (events, 13, 300);*/
 	
-	public void addTalk (Talks event) {
-		durationLeft = 180;
-		timeNeeded = event.getDuration();
-		while (durationLeft != 0) {
-			if (timeNeeded < durationLeft) {
+	public void addTalk (Talks event, Boolean nextExists) {
+
 				events.add(event);
-				durationLeft = durationLeft-timeNeeded;
-				System.out.println(events);
-			} else  {
-				System.out.println("Session voll");
-				break;
-			}
+				//durationLeft = durationLeft-timeNeeded;
+				System.out.println("hinzugefügt");
+				
+			} 
+	
+	/*@Override
+	  public String toString() {
+		String eventsString = events.toString();
+	    return event.name;
+	  }*/
+		
+		public void printList () {
+			System.out.println(events.toString());
 		}
-		
-		
 	}
 	
 	
 
-}
 
+
+/*
+ * 		durationLeft = 180;
+		timeNeeded = event.getDuration();
+		while (nextExists == true)
+			if (timeNeeded < durationLeft) {
+				//System.out.println(durationLeft);
+ */
 
 /*Data objt = new Data(name, address, contact); // Creating a new object
 Contacts.add(objt); // Adding it to the list*/
